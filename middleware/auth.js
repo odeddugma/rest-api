d943562b6d6a934ec469beb2ce1b9967de682e28
Oded Dugma <odeddugma@gmail.com>
2021-03-06 22:42:02 +0200

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,19 +4,14 @@ const config = require('config');
 module.exports = (req, res, next) => {
 
     const token = req.header('x-auth-token');
-
     if (!token) return res.status(401).send('Access denied. No token provided');
 
     try {
-
         const decoded = jwt.verify(token, config.get('jwtKey'));
         req.user = decoded;
         next();
 
     } catch (ex) {
-
         res.status(400).send('Invalid token.');
-
     }
-
 };
